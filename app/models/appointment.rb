3bf8b9d0968e59f1d0fa9f78f9fd8b156e6b819e
Chance Mims <chance@Chances-MacBook-Air.local>
2020-01-16 09:53:58 -0600

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,18 @@
+class Appointment < ApplicationRecord
+
+    belongs_to :doctor
+    belongs_to :patient
+
+    def format_date
+        self.appointment_datetime.strftime("%B %d, %Y  at %H:%M")
+    end
+
+    def patient_name
+        self.patient.name
+    end
+
+    def doctor_name
+        self.doctor.name
+    end
+
+end
